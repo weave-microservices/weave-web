4b71085b42aae46844f8985ccaf59eacec012169
Kevin Ries <kevin.ries@fachwerk.io>
2020-06-04 01:19:56 +0200

    Refactoring

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: [
+      'eslint-config-fw'
+    ],
+    rules: {
+      'quotes': [2, 'single', { 'avoidEscape': true }],
+      'indent': ['error', 2]
+    }
+  }
+  
